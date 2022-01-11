f97b7228aec294851c69fc06d5879ce7574b9edf
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2022-01-11 18:54:21 -0300

    arruma requisito 2

--- models/products/find.js
+++ models/products/find.js
@@ -4,7 +4,9 @@ const connect = require('../connection');
 module.exports = async (anything) => {
   const conn = await connect();
       
-  if (!anything) await conn.collection('products').find().toArray();
+  if (!anything) {
+   return await conn.collection('products').find().toArray();
+  }
 
   if (anything.name) {
     const foundProduct = await conn.collection('products').find({ name: anything.name }).toArray();
